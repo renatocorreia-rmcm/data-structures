b4c832fdc03d31cf154a69c02a39a7755320454e
renato correia <rmcm@cin.ufpe.br>
2025-02-15 17:59:35 -0300

    set default arguments

--- data_structures/node/node.hpp
+++ data_structures/node/node.hpp
@@ -6,7 +6,7 @@ class node {
 public:
     int data;
     node* next;
-    node(int new_data, node* next)
+    node(int new_data = 0, node* next = nullptr)
     {
         data = new_data;
         this->next = next;
