bd54a2836fdac452923074bbe2a9e4701dab4f75
renato correia <rmcm@cin.ufpe.br>
2025-02-15 18:00:38 -0300

    finished implementing queue

--- data_structures/queue/queue.hpp
+++ data_structures/queue/queue.hpp
@@ -6,21 +6,48 @@
 class queue
 {
 private:
-    
-public:
-    queue(){
+    node* head;
+    node* tail;
 
+public:
+
+    queue(){
+        head = tail = new node();
     }
 
     void push(int value){
-
+        node* new_node = new node(value);
+        tail->next = new_node;
+        tail = new_node;
     }
-    void pop(){
 
+    void pop(){
+        if (is_empty())
+        {
+            throw("queue underflow");
+        }
+
+        node* trash = head->next;
+        head->next = head->next->next;
+        delete (trash);
+
+        if (head->next == nullptr)
+        {
+            tail = head;
+        }
+        
     }
 
     int get_front(){
+        if (is_empty())
+        {
+            throw("queue underflow");
+        }
+        return head->next->data;
+    }
 
+    bool is_empty(){
+        return head->next == nullptr;
     }
 };
 
--- data_structures/queue/test.cpp
+++ data_structures/queue/test.cpp
@@ -10,8 +10,19 @@ int main(){
 
     queue q;
 
+    cout << q.is_empty() << '\n';
 
 
+    q.push(10);
+    cout << q.get_front() << '\n';
+
+    q.push(20);
+    cout << q.get_front() << '\n';
+
+    q.pop();
+    cout << q.get_front() << '\n';
+
+    cout << q.is_empty() << '\n';
 
 
     return 0;
