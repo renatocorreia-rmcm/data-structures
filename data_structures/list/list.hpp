3f5ed32def3b40d4a0692877c195b3333392f9d9
renato correia <rmcm@cin.ufpe.br>
2025-02-16 16:17:05 -0300

    finished implementing list
    stil can be aprimorated thoug. not so abstract and pratical

--- data_structures/list/list.hpp
+++ data_structures/list/list.hpp
@@ -23,34 +23,82 @@ public:
 
     list(){
         head = tail = curr = new node();
+        index = 0;
     }
 
 
-    void push(int value){
-        node* new_node = new node(value);
-        curr = new_node;
+    void insert(int value){
+        node* new_node = new node(value, curr->next);
+        curr->next = new_node;
 
+        if (tail == curr)  // element was inserted at tail - becomes new tail
+        {
+            tail = curr->next;
+        }
+
+        index++;
     }
 
     void remove(){
-        if (is_empty())
+        if (curr == tail)  // is last element or list is empty
         {
             throw("list underflow");
         }
 
-    }
+        node* trash = curr->next;
 
-    bool is_empty(){
-        return ;
-    }
-
-    int get_top(){
-        if (is_empty())
+        if (tail == trash)
         {
-            throw("list underflow");
+            tail = curr;
+        }
+
+        curr->next = curr->next->next;
+
+        delete trash;
+
+        index--;
+    }
+
+    void go_front(){
+        if (curr==tail)
+        {
+            throw("index out of range");
         }
         
-        return ;
+        curr = curr->next;
+    }
+    void go_back(bool to_head=false){
+        if (curr==head)
+        {
+            throw("index out of range");
+        }
+        if (to_head)
+        {
+            curr = head;
+            return;
+        }
+        
+
+        node* temp = head;
+        while (temp->next != curr)
+        {
+            temp=temp->next;
+        }
+
+        curr = temp;
+    }
+
+    int get_value(){
+        if (curr->next == nullptr)
+        {
+            throw("list underflow");
+        }
+        return curr->next->data;
+    }
+
+
+    bool is_empty(){
+        return head->next == nullptr;
     }
 
     void clear(){
--- data_structures/list/test.cpp
+++ data_structures/list/test.cpp
@@ -10,6 +10,30 @@ int main(){
 
     list l;
 
+    l.insert(1);
+    l.insert(2);
+    l.insert(3);
+
+
+    cout << l.get_value() << ' ';
+    l.go_front();
+    cout << l.get_value() << ' ';    
+    l.go_front();
+    cout << l.get_value() << ' ';
+    
+    l.go_back();
+    l.remove();
+
+    cout << '\n';
+
+    l.go_back(true);
+
+    cout << l.get_value() << ' ';
+    l.go_front();
+    cout << l.get_value() << ' ';    
+
+
+
 
     return 0;
 }
